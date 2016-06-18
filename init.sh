d8dd7beff1a1f83e795410546930f1d20c035b5f
General Ironwood <atlas@aurora-terraria.org>
2016-06-18 16:57:48 -0400

    wtf msys2

--- init.sh
+++ init.sh
@@ -2,35 +2,35 @@
 
 ## general options
 
-HUBOT_LOG_LEVEL='debug'
+export HUBOT_LOG_LEVEL='debug'
 
 ## slack related
 
-HUBOT_ADAPTER='slack'
-HUBOT_SLACK_TOKEN='xoxb-49962470917-gyJ35cOqBOKgyaf2Pqnh89oI'
-HUBOT_SLACK_EXIT_ON_DISCONNECT='true'
+export HUBOT_ADAPTER='slack'
+export HUBOT_SLACK_TOKEN='xoxb-49962470917-gyJ35cOqBOKgyaf2Pqnh89oI'
+export HUBOT_SLACK_EXIT_ON_DISCONNECT='true'
 
 ## brain
 
-HUBOT_FILE_BRAIN_PATH='brain.db'
+export HUBOT_FILE_BRAIN_PATH="C:\ProgramData\msys64\home\Administrator\arc-penny\brain.db"
 
 ## google shit
 
-HUBOT_GOOGLE_CSE_ID='015555699567081181253:x_vzxgf5kio'
-HUBOT_GOOGLE_CSE_KEY='AIzaSyDhh4v1GrAxHOafixcrAO1RYDKp_zan4bk'
-HUBOT_GOOGLE_SAFE_SEARCH='high'
+export HUBOT_GOOGLE_CSE_ID='015555699567081181253:x_vzxgf5kio'
+export HUBOT_GOOGLE_CSE_KEY='AIzaSyDhh4v1GrAxHOafixcrAO1RYDKp_zan4bk'
+export HUBOT_GOOGLE_SAFE_SEARCH='high'
 
 ## youtube
 
-HUBOT_YOUTUBE_API_KEY='AIzaSyDhh4v1GrAxHOafixcrAO1RYDKp_zan4bk'
-HUBOT_YOUTUBE_DETERMINISTIC_RESULTS='true'
+export HUBOT_YOUTUBE_API_KEY='AIzaSyDhh4v1GrAxHOafixcrAO1RYDKp_zan4bk'
+export HUBOT_YOUTUBE_DETERMINISTIC_RESULTS='true'
 
 ## imgur
 
-HUBOT_IMGUR_CLIENTID='f4facc7cbb59177'
+export HUBOT_IMGUR_CLIENTID='f4facc7cbb59177'
 
 ## auth.coffee admins: @newy @patrikk @clay @lillnex @clara
 
-HUBOT_AUTH_ADMIN='U04AMBT11,U0489BMSF,U1AL663F0,U0489C5N9,U048H7R95'
+export HUBOT_AUTH_ADMIN='U04AMBT11,U0489BMSF,U1AL663F0,U0489C5N9,U048H7R95'
 
-exec bin/hubot
+sh bin/hubot
